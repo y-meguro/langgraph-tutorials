a71f6bb560f4755880aadbe86fe8f558ec31bd5c
Yohei <y.meguro.2008@gmail.com>
2024-11-11 20:02:45 +0900

    feat: add quick_start

---
+++ README.md
@@ -0,0 +1,30 @@
+# LangGraph チュートリアル
+
+- [Tutorials](https://langchain-ai.github.io/langgraph/tutorials/) のコード
+
+## コマンド
+
+- ファイルの実行
+
+```
+poetry run python projects/simple_llm/main.py
+```
+
+- ライブラリの追加
+
+```
+poetry add {パッケージ名} --group {プロジェクト名}
+```
+
+- lint
+
+```
+poetry run ruff check
+poetry run ruff check --fix
+```
+
+- format
+
+```
+poetry run ruff format
+```
---
+++ projects/quick_start/part1.py
@@ -0,0 +1,53 @@
+from typing import Annotated
+
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langgraph.graph import END, START, StateGraph
+from langgraph.graph.message import add_messages
+from typing_extensions import TypedDict
+
+load_dotenv()
+
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+model = ChatOpenAI(model="gpt-3.5-turbo")
+
+
+def chatbot(state: State):
+    return {"messages": [model.invoke(state["messages"])]}
+
+
+# The first argument is the unique node name
+# The second argument is the function or object that will be called whenever
+# the node is used.
+graph_builder.add_node("chatbot", chatbot)
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile()
+
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [("user", user_input)]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        stream_graph_updates(user_input)
+    except Exception:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
